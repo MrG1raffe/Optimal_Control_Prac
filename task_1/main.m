2ca4e35ef8d068feecdf2c77a72340a2c18a7514
MrG1raffe <dmitrysotni@gmail.com>
2020-02-24 22:44:08 +0300

    Все дописал

--- task_1/main.m
+++ task_1/main.m
@@ -5,25 +5,160 @@ len = 6; % длина опорной гиперплоскости
 psi_0_n = 50; % размер сетки для psi_0
 n_check = 128; % количество направлений при проверке попадания в терминальное множество
 h_t = 0.001; % шаг сетки по времени
-T_max = 3; % максимально возможное t1
+T_max = 5; % максимально возможное t1
 B_det_eps = 0.00001; % эпсилон для определителя В 
 
 
-%% Описание входных данных
+
+%% Пример 1 (сразу хорошее УТ)
 A = [1 0; 1 1] / 10;
 B = [10 0; 0 10];
 f = [1; 1];
 a = 2;
 b = 1;
-x0 = [-3 7]';
-r0 = 0.5;
-x1 = [9 7 11 10;
-      3 4 5 7];
+x0 = [-4 7]';
+r0 = 1;
+x1 = [9 7 11 10 8;
+      3 4 5 7 8];
 eps = 1;
 
 while det(B) < B_det_eps
     B = B + (rand(2) - 0.5) * 10 * sqrt(B_det_eps);
 end
+
+figure('Name', 'Plot (x1, x2)');
+drawSet(@(l) rho_X0(l, x0, r0), drawSet_n, 'y');
+hold on;
+drawSet(@(l) rho_X1(l, x1, eps), drawSet_n, 'g');
+xlabel('x_1');
+ylabel('x_2');
+axis equal;
+
+
+%% Пример 2 (похож на 3)
+A = [0 1; 1 0];
+B = [5 0; 3 4];
+f = [0; 0];
+a = 4;
+b = 1;
+x0 = [0 0]';
+r0 = 0.7;
+x1 = [6 6 11 11 20 15;
+      4 10 10 0 4 15];
+x1 = x1 / 10 + [7; 7];
+eps = 1;
+
+while det(B) < B_det_eps
+    B = B + (rand(2) - 0.5) * 10 * sqrt(B_det_eps);
+end
+
+figure('Name', 'Plot (x1, x2)');
+drawSet(@(l) rho_X0(l, x0, r0), drawSet_n, 'y');
+hold on;
+drawSet(@(l) rho_X1(l, x1, eps), drawSet_n, 'g');
+xlabel('x_1');
+ylabel('x_2');
+axis equal;
+
+%% Пример 3 (Хорошее УТ на 2 итерации)
+A = [1 0; 0 -1];
+B = 2 * [5 1; 1 5];
+f = [-1; 1];
+a = 1;
+b = 1;
+x0 = [0 0]';
+r0 = 0.5;
+x1 = [10 13; 
+      10 13];
+x1 = x1 + [7; 0];
+eps = 1;
+
+while det(B) < B_det_eps
+    B = B + (rand(2) - 0.5) * 10 * sqrt(B_det_eps);
+end
+
+figure('Name', 'Plot (x1, x2)');
+drawSet(@(l) rho_X0(l, x0, r0), drawSet_n, 'y');
+hold on;
+drawSet(@(l) rho_X1(l, x1, eps), drawSet_n, 'g');
+xlabel('x_1');
+ylabel('x_2');
+axis equal;
+
+%% Пример 4 (фокус, 2 итерации)
+A = 2 * [1 -1; 1 1];
+B = [1 1; 2 0] / 3;
+f = [0; 0];
+a = 1;
+b = 1;
+x0 = [0 -1]';
+r0 = 1;
+x1 = [0; 
+      10];
+eps = 1;
+
+while det(B) < B_det_eps
+    B = B + (rand(2) - 0.5) * 10 * sqrt(B_det_eps);
+end
+
+figure('Name', 'Plot (x1, x2)');
+drawSet(@(l) rho_X0(l, x0, r0), drawSet_n, 'y');
+hold on;
+drawSet(@(l) rho_X1(l, x1, eps), drawSet_n, 'g');
+xlabel('x_1');
+ylabel('x_2');
+axis equal;
+
+%% Пример разрывности 1 (разрыв 2 рода)
+A = [1 0; 0 0];
+B = [1 0; 0 1];
+f = [-1; 0];
+a = 1;
+b = 1;
+x0 = [-1 0]';
+x0 = x0 + [0.1 0]';
+r0 = 1;
+x1 = [3; 
+      0];
+eps = 1;
+
+while det(B) < B_det_eps
+    B = B + (rand(2) - 0.5) * 10 * sqrt(B_det_eps);
+end
+
+figure('Name', 'Plot (x1, x2)');
+drawSet(@(l) rho_X0(l, x0, r0), drawSet_n, 'y');
+hold on;
+drawSet(@(l) rho_X1(l, x1, eps), drawSet_n, 'g');
+xlabel('x_1');
+ylabel('x_2');
+axis equal;
+
+%% Пример разрывности 2 (изменение времени скачком)
+A = 3 * [0 1; -1 0];
+B = [1 0; 0 1];
+f = [0; 0];
+a = 1;
+b = 1;
+x0 = [0 -2]';
+r0 = 1;
+x1 = [1; 
+      1] * 3 + 1/sqrt(2);
+x1 = x1 + 0.1 * [1; 1];
+eps = 1;
+
+while det(B) < B_det_eps
+    B = B + (rand(2) - 0.5) * 10 * sqrt(B_det_eps);
+end
+
+figure('Name', 'Plot (x1, x2)');
+drawSet(@(l) rho_X0(l, x0, r0), drawSet_n, 'y');
+hold on;
+drawSet(@(l) rho_X1(l, x1, eps), drawSet_n, 'g');
+xlabel('x_1');
+ylabel('x_2');
+axis equal;
+
 
 %% Вычисление траекторий, удовлетворяющих ПМП
 clc;
@@ -99,15 +234,15 @@ end
 if opt_ind
     plot(X(1, :, opt_ind), X(2, :, opt_ind), 'r', 'LineWidth', 2);
 end
-
+axis([-3 5 -3 3]);
 x_0 = X(:, 1, opt_ind);
 psi0 = psi_0(:, opt_ind);
 l = x_0 - x0;
 l = l / norm(l);
 tau = [-l(2); l(1)];
-plot(x_0(1) + linspace(-len/2, len/2, drawSet_n) * tau(1), x_0(2) + linspace(-len/2, len/2, drawSet_n) * tau(2), 'black', 'LineWidth', 2);
+%plot(x_0(1) + linspace(-len/2, len/2, drawSet_n) * tau(1), x_0(2) + linspace(-len/2, len/2, drawSet_n) * tau(2), 'black', 'LineWidth', 2);
 hold on;
-plot(x_0(1) + linspace(0, 1, drawSet_n) * psi0(1), x_0(2) + linspace(0, 1, drawSet_n) * psi0(2), 'black', 'LineWidth', 2);
+%plot(x_0(1) + linspace(0, 1, drawSet_n) * psi0(1), x_0(2) + linspace(0, 1, drawSet_n) * psi0(2), 'black', 'LineWidth', 2);
 hold on;
 
 x_1 = X(:, size(X, 2), opt_ind);
@@ -115,36 +250,34 @@ psi1 = Psi(:, size(Psi, 2), opt_ind);
 psi1 = psi1 / norm(psi1);
 l1 = find_dir(x_1, @(l) rho_X1(l, x1, eps), n_check);
 tau = [-l1(2); l1(1)];
-plot(x_1(1) + linspace(-len/2, len/2, drawSet_n) * tau(1), x_1(2) + linspace(-len/2, len/2, drawSet_n) * tau(2), 'black', 'LineWidth', 2);
+%plot(x_1(1) + linspace(-len/2, len/2, drawSet_n) * tau(1), x_1(2) + linspace(-len/2, len/2, drawSet_n) * tau(2), 'black', 'LineWidth', 2);
 hold on;
-plot(x_1(1) + linspace(0, 1, drawSet_n) * (-psi1(1)), x_1(2) + linspace(0, 1, drawSet_n) * (-psi1(2)), 'black', 'LineWidth', 2);
+%plot(x_1(1) + linspace(0, 1, drawSet_n) * (-psi1(1)), x_1(2) + linspace(0, 1, drawSet_n) * (-psi1(2)), 'black', 'LineWidth', 2);
 hold on;
+
 %% График (t, x1)
 figure('Name', 'Plot (t, x1)');
-xlabel('t');
-ylabel('x1');
-axis equal;
+
 
 for i = 1:psi_0_n
     plot(t, X(1, :, i), 'b');
     hold on;
 end
-
+xlabel('t');
+ylabel('x1');
 if opt_ind
     plot(t, X(1, :, opt_ind), 'r', 'LineWidth', 2);
 end
 
 %% График (t, x2)
 figure('Name', 'Plot (t, x2)');
-xlabel('t');
-ylabel('x2');
-axis equal;
 
 for i = 1:psi_0_n
     plot(t, X(2, :, i), 'b');
     hold on;
 end
-
+xlabel('t');
+ylabel('x2');
 if opt_ind
     plot(t, X(2, :, opt_ind), 'r', 'LineWidth', 2);
 end
@@ -161,14 +294,13 @@ axis equal;
 
 %% График (t, u1)
 figure('Name', 'Plot (t, u1)');
-xlabel('t');
-ylabel('u1');
-axis equal;
 
 for i = 1:psi_0_n
     plot(t, U(1, :, i), 'b');
     hold on;
 end
+xlabel('t');
+ylabel('u1');
 
 if opt_ind
     plot(t, U(1, :, opt_ind), 'r', 'LineWidth', 2);
@@ -176,14 +308,14 @@ end
 
 %% График (t, u2)
 figure('Name', 'Plot (t, u2)');
-xlabel('t');
-ylabel('u2');
-axis equal;
+
 
 for i = 1:psi_0_n
     plot(t, U(2, :, i), 'b');
     hold on;
 end
+xlabel('t');
+ylabel('u2');
 
 if opt_ind
     plot(t, U(2, :, opt_ind), 'r', 'LineWidth', 2);
@@ -191,28 +323,28 @@ end
 
 %% График (psi1, psi2)
 figure('Name', 'Plot (psi1, psi2)');
-xlabel('psi_1');
-ylabel('psi_2');
+
 
 for i = 1:psi_0_n
     plot(Psi(1, :, i), Psi(2, :, i), 'b');
     hold on;
 end
-
+xlabel('\psi_1');
+ylabel('\psi_2');
 if opt_ind
     plot(Psi(1, :, opt_ind), Psi(2, :, opt_ind), 'r', 'LineWidth', 2);
 end
 
 %% График (t, psi1)
 figure('Name', 'Plot (t, psi1)');
-xlabel('t');
-ylabel('psi1');
-axis equal;
+
 
 for i = 1:psi_0_n
     plot(t, Psi(1, :, i), 'b');
     hold on;
 end
+xlabel('t');
+ylabel('\psi_1');
 
 if opt_ind
     plot(t, Psi(1, :, opt_ind), 'r', 'LineWidth', 2);
@@ -220,15 +352,13 @@ end
 
 %% График (t, psi2)
 figure('Name', 'Plot (t, psi2)');
-xlabel('t');
-ylabel('psi2');
-axis equal;
 
 for i = 1:psi_0_n
     plot(t, Psi(2, :, i), 'b');
     hold on;
 end
-
+xlabel('t');
+ylabel('\psi_2');
 if opt_ind
     plot(t, Psi(2, :, opt_ind), 'r', 'LineWidth', 2);
 end
